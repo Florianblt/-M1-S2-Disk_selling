50c30f8387dbe089b5c107579a6d7379c7c2553b
Unknown <Florianblot059@gmail.com>
2018-03-15 15:46:25 +0100

    First commit

---
+++ src/main/java/com/dev/selldisk/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package com.dev.selldisk.controller;
+
+public class ArticleController {
+
+    private int idArticle;
+    private String titre;
+    private String description;
+    private String type;
+    private float prix;
+
+
+
+}
---
+++ src/main/java/com/dev/selldisk/model/Article.java
@@ -0,0 +1,4 @@
+package com.dev.selldisk.model;
+
+public class Article {
+}
