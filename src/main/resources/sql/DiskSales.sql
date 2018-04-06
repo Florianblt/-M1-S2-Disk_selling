8c49c28dcfe4b11903a258bb3a92c04607bc09d2
Unknown <Florianblot059@gmail.com>
2018-04-06 09:01:11 +0200

    Change disksale.sql

--- src/main/resources/sql/DiskSales.sql
+++ src/main/resources/sql/DiskSales.sql
@@ -1,35 +1,52 @@
+DROP TABLE IF EXISTS LigneArticle CASCADE;
+DROP TABLE IF EXISTS Format CASCADE;
 DROP TABLE IF EXISTS Article CASCADE;
 DROP TABLE IF EXISTS Liste CASCADE;
 DROP TABLE IF EXISTS Client CASCADE;
 
 CREATE TABLE Client
 (
-    idClient serial NOT NULL,
-    nom varchar(100) NOT NULL,
-    mail varchar(100) NOT NULL,
-    password varchar(100) NOT NULL,
-    PRIMARY KEY (idClient)
+  idClient serial NOT NULL,
+  nom varchar(100) NOT NULL,
+  mail varchar(100) NOT NULL,
+  password varchar(100) NOT NULL,
+  PRIMARY KEY (idClient)
 );
 
 CREATE TABLE Liste
 (
-    idListe serial NOT NULL,
-    date Date NOT NULL,
-    etat int NOT NULL,
-    total numeric(8,2) NOT NULL,
-    idClient int NOT NULL,
-    PRIMARY KEY (idListe),
-    FOREIGN KEY (idClient) REFERENCES Client(idClient)
+  idListe serial NOT NULL,
+  date Date NOT NULL,
+  etat int NOT NULL,
+  total numeric(8,2) NOT NULL,
+  idClient int NOT NULL,
+  PRIMARY KEY (idListe),
+  FOREIGN KEY (idClient) REFERENCES Client(idClient)
 );
 
 CREATE TABLE Article
 (
-    idArticle serial NOT NULL,
-    titre varchar(100) NOT NULL,
-    description varchar(255) NOT NULL,
-    type varchar(100) NOT NULL,
-    prix numeric(8,2) NOT NULL,
-    PRIMARY KEY (idArticle)
+  idArticle serial NOT NULL,
+  titre varchar(100) NOT NULL,
+  description varchar(255) NOT NULL,
+  prix numeric(8,2) NOT NULL,
+  PRIMARY KEY (idArticle)
 );
 
-INSERT INTO Article VALUES (DEFAULT,'Article de test','Description de test','Disque',20);
+CREATE Table Format
+(
+  idFormat serial NOT NULL,
+  idArticle int NOT NULL references Article,
+  libelleFormat varchar(100) NOT NULL,
+  PRIMARY KEY(idFormat)
+);
+
+CREATE Table LigneArticle
+(
+  idArticle int references Article,
+  idListe int references Liste,
+  quantite int NOT NULL,
+  primary key (idArticle, idListe)
+);
+INSERT INTO Article VALUES (DEFAULT,'Article de test','Description de test',20);
+INSERT INTO Format VALUES (DEFAULT, 1, 'Disque');
