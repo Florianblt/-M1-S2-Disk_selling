4263c78db74485641d8f9e5c8080030da0cb9566
Jean-Alexandre <jannou59@gmail.com>
2018-04-26 18:44:28 +0200

    add material design on front

--- front/sell-disk-app/src/app/app.module.ts
+++ front/sell-disk-app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 
 import {ArticleComponent} from "./article/article.component";
 import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MaterialModule} from "./material.module";
 
 
 @NgModule({
@@ -18,7 +20,9 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
