bca21e2bcd748b13800adab987b9333b92b932a1
Jean-Alexandre <jannou59@gmail.com>
2018-04-27 10:31:23 +0200

    feat(article): creation template presentation article

--- front/sell-disk-app/src/app/article/article.component.ts
+++ front/sell-disk-app/src/app/article/article.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Article} from '../models/article';
 import {HttpClient} from "@angular/common/http";
+import {ArticleService} from "../services/article-service.service";
 
 @Component({
   selector: 'app-article',
@@ -10,15 +11,29 @@ import {HttpClient} from "@angular/common/http";
 export class ArticleComponent implements OnInit {
 
   article: any;
+  nbArticle: number;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private articleService: ArticleService) {
+    this.nbArticle = 0;
 
   }
 
   ngOnInit() {
-    this.http.get('http://localhost:8080/article/get/1').subscribe(data => {
-      this.article = data;
-    });
+    // this.http.get('http://localhost:8080/article/get/1').subscribe(data => {
+    //   this.article = data;
+    // });
+    this.article = this.articleService.myData();
+  }
+
+  public addArticle(){
+    this.nbArticle += 1;
+  }
+
+  public removeArticle() {
+    if(this.nbArticle > 0) {
+      this.nbArticle -= 1;
+    }
   }
 
 }
--- front/sell-disk-app/src/app/services/article-service.service.ts
+++ front/sell-disk-app/src/app/services/article-service.service.ts
@@ -6,10 +6,7 @@ export class ArticleService {
   private article: Article;
 
   constructor() {
-    this.article.idArticle = 1;
-    this.article.titre = 'La vrai vie';
-    this.article.description = 'Album de Big Flo & Oli'
-    this.article.prix = 14.99;
+    this.article = {idArticle: 1, titre: 'La vrai vie', description: 'Album de Big Flo & Oli', prix: 14.99};
   }
 
   myData() {
